5ec38f76cb0c86fa33464803d9ea497e9c5c5235
A. Koepcke <akoepcke@users.noreply.github.com>
2021-09-14 14:57:01 +0200

    WIP

--- pages/Recorder/recorder.js
+++ pages/Recorder/recorder.js
@@ -1,28 +1,52 @@
 import { useRef, useEffect } from "react";
 import { ReactMediaRecorder } from "react-media-recorder";
 
+const VideoPreview = ({ stream }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current && stream) {
+      videoRef.current.srcObject = stream;
+    }
+  }, [stream]);
+  if (!stream) {
+    return null;
+  }
+  return <video ref={videoRef} width={500} height={500} autoPlay controls />;
+};
+
 const RecordView = () => {
 
   return (
    <div className="p-4">
-      <ReactMediaRecorder
-        video
-        audio={false}
-        blobPropertyBag={{
-          type: "video/mp4"
-       }}
-       askPermissionOnMount={true}
-        render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-          <div>
-            <p>status: {status}</p>
-            <div className="grid grid-cols-2 gap-2 my-4">
-              <button onClick={startRecording} className="p-2 border border-gray-500 rounded">Start Recording</button>
-              <button onClick={stopRecording} className="p-2 border border-gray-500 rounded">Stop Recording</button>
+     <div className="border-2">
+        <ReactMediaRecorder
+          video
+          audio={false}
+          blobPropertyBag={{
+            type: "video/mp4"
+         }}
+         askPermissionOnMount={true}
+          render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+            <div>
+              <p>status: {status}</p>
+              <div className="grid grid-cols-2 gap-2 my-4">
+                <button onClick={startRecording} className="p-2 border border-gray-500 rounded">Start Recording</button>
+                <button onClick={stopRecording} className="p-2 border border-gray-500 rounded">Stop Recording</button>
+              </div>
+              <video src={mediaBlobUrl} controls autoPlay />
             </div>
-            <video src={mediaBlobUrl} controls autoPlay loop />
-          </div>
-        )}
-      />
+          )}
+        />
+     </div>
+     <div className="border-2">
+        <ReactMediaRecorder
+      video
+      render={({ previewStream }) => {
+        return <VideoPreview stream={previewStream} />;
+      }}
+    />
+     </div>
    </div>
   );
 };
