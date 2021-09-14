9c020238ab77e9745ee2f59e28fbbf8e7c279c73
A. Koepcke <akoepcke@users.noreply.github.com>
2021-09-14 15:05:35 +0200

    WIP

--- pages/Recorder/recorder.js
+++ pages/Recorder/recorder.js
@@ -1,20 +1,6 @@
 import { useRef, useEffect } from "react";
 import { ReactMediaRecorder } from "react-media-recorder";
 
-const VideoPreview = ({ stream }) => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    if (videoRef.current && stream) {
-      videoRef.current.srcObject = stream;
-    }
-  }, [stream]);
-  if (!stream) {
-    return null;
-  }
-  return <video ref={videoRef} width={500} height={500} autoPlay controls />;
-};
-
 const RecordView = () => {
 
   return (
@@ -27,6 +13,7 @@ const RecordView = () => {
             type: "video/mp4"
          }}
          askPermissionOnMount={true}
+         mediaRecorderOptions={{facingMode: "environment"}}
          facingMode="environment"
           render={({ status, startRecording, stopRecording, mediaBlobUrl, previewStream }) => (
             <div>
